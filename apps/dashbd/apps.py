ba4fc5add9847cdd4f3419e48d55c070b343ecc7
giovanni-angeli <giovanni.angeli6@google.com>
2018-02-19 16:02:54 +0100

    refactor and activate jsoneditor based on jsonschema

--- apps/dashbd/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DashbdConfig(AppConfig):
-    name = 'dashbd'
--- apps/fermions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FermionsConfig(AppConfig):
-    name = 'fermions'
